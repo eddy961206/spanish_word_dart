f5ca104210cbb48e96484d325509dc9e93b1c0f2
ryu <skykum2004@gmail.com>
2023-07-29 04:54:58 +0900

    코드 개선

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:spanish_word/word.dart';
 import 'package:spanish_word/word_display.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
-  await dotenv.load();
   runApp(MyApp());
 }
-
-// GlobalKey를 생성합니다.
-final wordFetchStateKey = GlobalKey<_WordFetchStateState>();
 
 class MyApp extends StatelessWidget {
   @override
@@ -22,19 +17,9 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Word of the Day'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: () {
-                // GlobalKey를 통해 WordFetchState의 상태를 가져와 refreshWord 메서드를 호출합니다.
-                wordFetchStateKey.currentState!.refreshWord();
-              },
-            ),
-          ],
         ),
         body: Center(
-          // WordFetchState 위젯에 GlobalKey를 할당합니다.
-          child: WordFetchState(key: wordFetchStateKey),
+          child: WordFetchState(),
         ),
       ),
     );
@@ -57,7 +42,6 @@ class _WordFetchStateState extends State<WordFetchState> {
     _futureWord = fetchWord();
   }
 
-  // 새로고침 기능을 위한 메서드를 추가합니다.
   void refreshWord() {
     setState(() {
       _futureWord = fetchWord();
@@ -66,16 +50,26 @@ class _WordFetchStateState extends State<WordFetchState> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Word>(
-      future: _futureWord,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return WordDisplay(word: snapshot.data!);
-        } else if (snapshot.hasError) {
-          return Text("${snapshot.error}");
-        }
-        return CircularProgressIndicator();
-      },
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: FutureBuilder<Word>(
+            future: _futureWord,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return WordDisplay(word: snapshot.data!);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+        FloatingActionButton(
+          onPressed: refreshWord,
+          child: Icon(Icons.refresh),
+        ),
+      ],
     );
   }
 }
